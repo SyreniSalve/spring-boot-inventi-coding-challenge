d084552872ef7e6281cf255b9beae649741dbb07
Simona Liamo <liamosimona@yahoo.com>
2022-06-10 00:13:55 +0300

    start challenge

---
+++ src/main/java/com/example/springbootinventicodingchallenge/SpringBootInventiCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootinventicodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInventiCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInventiCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootinventicodingchallenge/SpringBootInventiCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootinventicodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInventiCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
